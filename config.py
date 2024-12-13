ad94cd460337353928d8511bb6fcd888d1d579a1
minhduc <91581808+Balicki24@users.noreply.github.com>
2024-12-13 19:21:07 +0700

    fix bug in train.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-model_name = os.environ['MODEL_NAME'] if 'MODEL_NAME' in os.environ else 'NRMS'
+model_name = os.environ['MODEL_NAME'] if 'MODEL_NAME' in os.environ else 'NAML'
 # Currently included model
 assert model_name in [
     'NRMS', 'NAML', 'LSTUR', 'DKN', 'HiFiArk', 'TANR', 'Exp1'
--- train.py
+++ train.py
@@ -14,8 +14,10 @@ from evaluate import evaluate
 import importlib
 import datetime
 
+
+
 try:
-    Model = getattr(importlib.import_module(f"model.{model_name}"), model_name)
+    Model = getattr(importlib.import_module("NAML_Model"), model_name)
     config = getattr(importlib.import_module('config'), f"{model_name}Config")
 except AttributeError:
     print(f"{model_name} not included!")
@@ -79,36 +81,9 @@ def train():
     except FileNotFoundError:
         pretrained_word_embedding = None
 
-    if model_name == 'DKN':
-        try:
-            pretrained_entity_embedding = torch.from_numpy(
-                np.load(
-                    './data/train/pretrained_entity_embedding.npy')).float()
-        except FileNotFoundError:
-            pretrained_entity_embedding = None
-
-        try:
-            pretrained_context_embedding = torch.from_numpy(
-                np.load(
-                    './data/train/pretrained_context_embedding.npy')).float()
-        except FileNotFoundError:
-            pretrained_context_embedding = None
-
-        model = Model(config, pretrained_word_embedding,
-                      pretrained_entity_embedding,
-                      pretrained_context_embedding).to(device)
-    elif model_name == 'Exp1':
-        models = nn.ModuleList([
-            Model(config, pretrained_word_embedding).to(device)
-            for _ in range(config.ensemble_factor)
-        ])
-    else:
-        model = Model(config, pretrained_word_embedding).to(device)
-
-    if model_name != 'Exp1':
-        print(model)
-    else:
-        print(models[0])
+    # Initialize only NAML model
+    model = Model(config, pretrained_word_embedding).to(device)
+    print(model)
 
     dataset = BaseDataset('data/train/behaviors_parsed.tsv',
                           'data/train/news_parsed.tsv')
@@ -122,16 +97,10 @@ def train():
                    num_workers=config.num_workers,
                    drop_last=True,
                    pin_memory=True))
-    if model_name != 'Exp1':
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(),
-                                     lr=config.learning_rate)
-    else:
-        criterion = nn.NLLLoss()
-        optimizers = [
-            torch.optim.Adam(model.parameters(), lr=config.learning_rate)
-            for model in models
-        ]
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+
     start_time = time.time()
     loss_full = []
     exhaustion_count = 0
@@ -147,16 +116,9 @@ def train():
         checkpoint = torch.load(checkpoint_path)
         early_stopping(checkpoint['early_stop_value'])
         step = checkpoint['step']
-        if model_name != 'Exp1':
-            model.load_state_dict(checkpoint['model_state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            model.train()
-        else:
-            for model in models:
-                model.load_state_dict(checkpoint['model_state_dict'])
-                model.train()
-            for optimizer in optimizers:
-                optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        model.train()
 
     for i in tqdm(range(
             1,
@@ -179,61 +141,15 @@ def train():
             minibatch = next(dataloader)
 
         step += 1
-        if model_name == 'LSTUR':
-            y_pred = model(minibatch["user"], minibatch["clicked_news_length"],
-                           minibatch["candidate_news"],
-                           minibatch["clicked_news"])
-        elif model_name == 'HiFiArk':
-            y_pred, regularizer_loss = model(minibatch["candidate_news"],
-                                             minibatch["clicked_news"])
-        elif model_name == 'TANR':
-            y_pred, topic_classification_loss = model(
-                minibatch["candidate_news"], minibatch["clicked_news"])
-        elif model_name == 'Exp1':
-            y_preds = [
-                model(minibatch["candidate_news"], minibatch["clicked_news"])
-                for model in models
-            ]
-            y_pred_averaged = torch.stack(
-                [F.softmax(y_pred, dim=1) for y_pred in y_preds],
-                dim=-1).mean(dim=-1)
-            y_pred = torch.log(y_pred_averaged)
-        else:
-            y_pred = model(minibatch["candidate_news"],
-                           minibatch["clicked_news"])
+        y_pred = model(minibatch["candidate_news"], minibatch["clicked_news"])
 
         y = torch.zeros(len(y_pred)).long().to(device)
         loss = criterion(y_pred, y)
-
-        if model_name == 'HiFiArk':
-            if i % 10 == 0:
-                writer.add_scalar('Train/BaseLoss', loss.item(), step)
-                writer.add_scalar('Train/RegularizerLoss',
-                                  regularizer_loss.item(), step)
-                writer.add_scalar('Train/RegularizerBaseRatio',
-                                  regularizer_loss.item() / loss.item(), step)
-            loss += config.regularizer_loss_weight * regularizer_loss
-        elif model_name == 'TANR':
-            if i % 10 == 0:
-                writer.add_scalar('Train/BaseLoss', loss.item(), step)
-                writer.add_scalar('Train/TopicClassificationLoss',
-                                  topic_classification_loss.item(), step)
-                writer.add_scalar(
-                    'Train/TopicBaseRatio',
-                    topic_classification_loss.item() / loss.item(), step)
-            loss += config.topic_classification_loss_weight * topic_classification_loss
         loss_full.append(loss.item())
-        if model_name != 'Exp1':
-            optimizer.zero_grad()
-        else:
-            for optimizer in optimizers:
-                optimizer.zero_grad()
+
+        optimizer.zero_grad()
         loss.backward()
-        if model_name != 'Exp1':
-            optimizer.step()
-        else:
-            for optimizer in optimizers:
-                optimizer.step()
+        optimizer.step()
 
         if i % 10 == 0:
             writer.add_scalar('Train/Loss', loss.item(), step)
@@ -244,11 +160,10 @@ def train():
             )
 
         if i % config.num_batches_validate == 0:
-            (model if model_name != 'Exp1' else models[0]).eval()
+            model.eval()
             val_auc, val_mrr, val_ndcg5, val_ndcg10 = evaluate(
-                model if model_name != 'Exp1' else models[0], './data/val',
-                config.num_workers, 200000)
-            (model if model_name != 'Exp1' else models[0]).train()
+                model, './data/val', config.num_workers, 200000)
+            model.train()
             writer.add_scalar('Validation/AUC', val_auc, step)
             writer.add_scalar('Validation/MRR', val_mrr, step)
             writer.add_scalar('Validation/nDCG@5', val_ndcg5, step)
@@ -265,15 +180,10 @@ def train():
                 try:
                     torch.save(
                         {
-                            'model_state_dict': (model if model_name != 'Exp1'
-                                                 else models[0]).state_dict(),
-                            'optimizer_state_dict':
-                            (optimizer if model_name != 'Exp1' else
-                             optimizers[0]).state_dict(),
-                            'step':
-                            step,
-                            'early_stop_value':
-                            -val_auc
+                            'model_state_dict': model.state_dict(),
+                            'optimizer_state_dict': optimizer.state_dict(),
+                            'step': step,
+                            'early_stop_value': -val_auc
                         }, f"./checkpoint/{model_name}/ckpt-{step}.pth")
                 except OSError as error:
                     print(f"OS error: {error}")
